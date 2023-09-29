10bfc89de91c8ca22240fd35412717b94fe590f7
ttipoya <124883294+ttipoya@users.noreply.github.com>
2023-09-29 14:52:50 +0400

    Update 55.py

--- 55.py
+++ 55.py
@@ -1,9 +1,6 @@
-from itertools import product
-from random import randint
-import re
 from tkinter import *
-from tkinter import ttk
-
+import tkinter as tk
+from tkinter import messagebox
 
 root = Tk()
 w = root.winfo_screenwidth()
@@ -25,40 +22,61 @@ canvas.create_rectangle(25, 278, 375, 22, fill="#CDB5CD",outline="#8B7B8B")
 
 log = StringVar()
 pas = StringVar()
-def close(error):
-    error.grab_release()
-    error.destroy()
+enabled = IntVar()
+def paf(par_ent):
+    if enabled.get() == 1:
+        par_ent['show'] = NONE
+    if enabled.get() == 0:
+        par_ent['show'] = '*'
+class CreateToolTip(object):
+    def __init__(self, widget, text='widget info'):
+        self.waittime = 500
+        self.wraplength = 180
+        self.widget = widget
+        self.text = text
+        self.widget.bind("<Enter>", self.enter)
+        self.widget.bind("<Leave>", self.leave)
+        self.widget.bind("<ButtonPress>", self.leave)
+        self.id = None
+        self.tw = None
+    def enter(self, event=None):
+        self.schedule()
+    def leave(self, event=None):
+        self.unschedule()
+        self.hidetip()
+    def schedule(self):
+        self.unschedule()
+        self.id = self.widget.after(self.waittime, self.showtip)
+    def unschedule(self):
+        id = self.id
+        self.id = None
+        if id:
+            self.widget.after_cancel(id)
+    def showtip(self, event=None):
+        x = y = 0
+        x, y, cx, cy = self.widget.bbox("insert")
+        x += self.widget.winfo_rootx() + 30
+        y += self.widget.winfo_rooty() + 25
+        self.tw = tk.Toplevel(self.widget)
+        self.tw.wm_overrideredirect(True)
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(self.tw, text=self.text, justify='left',
+                       background="#ffffff", relief='solid', borderwidth=1,
+                       wraplength = self.wraplength)
+        label.pack(ipadx=1)
+    def hidetip(self):
+        tw = self.tw
+        self.tw= None
+        if tw:
+            tw.destroy()
 def da():
     ex = Tk()
-    ud = Toplevel()
-    ud.attributes("-topmost", True)
     ex.resizable(False, False)
-    ud.resizable(False, False)
     ex.geometry(f'500x500+{wh}+{hh}')
-    ud.geometry(f'300x100+{wh+100}+{hh+200}')
-    canvas = Canvas(ud, bg="#CDB5CD", width=300, height=100)
-    canvas.pack(anchor=CENTER, expand=1)
-    canvas.create_line(0, 300, 400, 0, fill="#8B7B8B", width=200, smooth=TRUE)
-    canvas.create_text(150, 35, text="Вы успешно прошли\nавторизацию/регистрацию", font="Courier 10", fill="#282828")
-    ud.grab_set()
-    btn = Button(ud, text="Закрыть", command=lambda: close(ud), bg="#CDB5CD", fg="#282828",
-                 activebackground="#8B7B8B")
-    canvas.create_window(60, 60, anchor=NW, window=btn, width=180, height=30)
+    messagebox.showinfo("Всё верно", 'Успешная регистрация/авторизация')
 def check():
-    if (len(log.get()) and len(pas.get())) == 0 or (len(log.get()) or len(pas.get())) == 0:
-        error = Toplevel()
-        error.attributes("-topmost", True)
-        error.resizable(False, False)
-        error.title("ВНИМАНИЕ")
-        error.geometry(f'300x100+{wh+50}+{hh+100}')
-        canvas = Canvas(error, bg="#CDB5CD", width=400, height=300)
-        canvas.pack(anchor=CENTER, expand=1)
-        canvas.create_line(0, 300, 400, 0, fill="#8B7B8B", width=200, smooth=TRUE)
-        canvas.create_text(155,35, text="НЕКОРРЕКТНОЕ ЗНАЧЕНИЕ", font="Courier 15", fill="#282828")
-        btn = Button(error, text="Закрыть", command = lambda: close(error), bg="#CDB5CD", fg="#282828",
-                     activebackground="#8B7B8B")
-        canvas.create_window(60, 60, anchor=NW, window=btn, width=180, height=30)
-        error.grab_set()
+    if (len(log.get()) and len(pas.get())) == 0 or ((len(log.get()) or len(pas.get())) == 0) or (len(pas.get())> 20 or len(pas.get())<8) or log.get().count(' ')> 0 or pas.get().count(' ')> 0:
+        messagebox.showerror("Ошибка",'Некорректные данные')
     else:
         root.destroy()
         da()
@@ -68,8 +86,11 @@ def reg():
     log_ent = Entry(font="Courier 15",textvariable=log,bg ="#CDB5CD",fg="#282828")
     canvas.create_window(180, 127, anchor=NW, window=log_ent, width=125, height=20)
     canvas.create_text(123, 175, text="Пароль:", font="Courier 20",fill="#282828")
-    par_ent = Entry(font="Courier 15",textvariable=pas,bg ="#CDB5CD",show="*")
+    par_ent = Entry(root,font="Courier 15",textvariable=pas,bg ="#CDB5CD",show="*")
     canvas.create_window(180, 166, anchor=NW, window=par_ent, width=125, height=20)
+    button1_ttp = CreateToolTip(par_ent, 'Введите количество символов от 8 до 20 без пробела')
+    enabled_checkbutton = Checkbutton(text="",bg ="#CDB5CD" ,activebackground="#CDB5CD", variable= enabled, command= lambda :paf(par_ent))
+    canvas.create_window(310, 166, anchor=NW, window=enabled_checkbutton, width=20, height=20)
     btn = Button(root,text="Авторизация/Регистрация",command=check,bg ="#CDB5CD",fg="#282828",activebackground="#8B7B8B")
     canvas.create_window(110, 210, anchor=NW, window=btn, width=180, height=30)
 reg()
